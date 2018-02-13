36ab9d4e2c33991946b8e0201a25fa9f91922f72
danielRicaud <ricaud512@gmail.com>
2018-02-13 15:51:41 -0600

    First attempt at rendering graphs on early payments page

--- src/app/components/screens/EarlyPayments/index.js
+++ src/app/components/screens/EarlyPayments/index.js
@@ -20,7 +20,9 @@ import GreenCloud from '../../../style/images/GreenCloud.png';
 import DoctorSplash from '../../../style/images/doctorsplashkeyboard.jpg';
 import styled from 'styled-components';
 import axios from 'axios';
-import "./grid.scss";
+import "./grid.css";
+import { VictoryChart, VictoryBar, VictoryLabel, VictoryAxis, Bar, VictoryTheme, VictorySharedEvents, VictoryPie, VictoryGroup, VictoryArea } from 'victory';
+
 //
 // import XLSX from 'xlsx';
 //
@@ -65,20 +67,131 @@ class EarlyPayments extends Component {
 
     let amountSorted = response.data[0].slice();
     let daysSorted = response.data[0].slice();
+    let impactSorted = response.data[0].slice();
+
+    let daysVsAmount = [];
+    let daysVsImpact = [];
+
+    let daysVsAmountLine = [];
+    let daysVsImpactLine = [];
+
+    let amountUnder20 = [];
+    let amountUnder40 = [];
+    let amountUnder60 = [];
+    let amountUnder80 = [];
+    let amountUnder100 = [];
+
+    // let daysUnder20 = [];
+    // let daysUnder40 = [];
+    // let daysUnder60 = [];
+    // let daysUnder80 = [];
+    // let daysUnder100 = [];
+
+    // let impactUnder20 = [];
+    // let impactUnder40 = [];
+    // let impactUnder60 = [];
+    // let impactUnder80 = [];
+    // let impactUnder100 = [];
+
+    let amountUnder20Counter = 0;
+    let amountUnder40Counter = 0;
+    let amountUnder60Counter = 0;
+    let amountUnder80Counter = 0;
+    let amountUnder100Counter = 0;
+
+    // let daysUnder20Counter = 0;
+    // let daysUnder40Counter = 0;
+    // let daysUnder60Counter = 0;
+    // let daysUnder80Counter = 0;
+    // let daysUnder100Counter = 0;
+
+    let impactUnder20Counter = 0;
+    let impactUnder40Counter = 0;
+    let impactUnder60Counter = 0;
+    let impactUnder80Counter = 0;
+    let impactUnder100Counter = 0;
 
 
 
-    amountSorted.sort((a, b) => {
-      return a.INV_PAYMENT_AMT - b.INV_PAYMENT_AMT;
-    });
+
+
+    // amountSorted.sort((a, b) => {
+    //   return a.INV_PAYMENT_AMT - b.INV_PAYMENT_AMT;
+    // });
 
     daysSorted.sort((a, b) => {
       return a.DAYS_PAID_EARLY - b.DAYS_PAID_EARLY;
     });
 
-    
+    // impactSorted.sort((a, b) => {
+    //   return a.NEGATIVE_CASH_IMPACT - b.NEGATIVE_CASH_IMPACT;
+    // });
 
-    console.log(amountSorted[603]);
+    daysSorted.forEach((element, index) => {
+      if(index <= (amountSorted.length * 0.2)){
+        amountUnder20.push(element);
+        amountUnder20Counter += element.INV_PAYMENT_AMT;
+        impactUnder20Counter += element.NEGATIVE_CASH_IMPACT;
+      }
+      else if(index <= (amountSorted.length * 0.4)){
+        amountUnder40.push(element);
+        amountUnder40Counter += element.INV_PAYMENT_AMT;
+        impactUnder40Counter += element.NEGATIVE_CASH_IMPACT;
+      }
+      else if(index <= (amountSorted.length * 0.6)){
+        amountUnder60.push(element);
+        amountUnder60Counter += element.INV_PAYMENT_AMT;
+        impactUnder60Counter += element.NEGATIVE_CASH_IMPACT;
+      }
+      else if(index <= (amountSorted.length * 0.8)){
+        amountUnder80.push(element);
+        amountUnder80Counter += element.INV_PAYMENT_AMT;
+        impactUnder80Counter += element.NEGATIVE_CASH_IMPACT;
+      }
+      else{
+        amountUnder100.push(element);
+        amountUnder100Counter += element.INV_PAYMENT_AMT;
+        impactUnder100Counter += element.NEGATIVE_CASH_IMPACT;
+      }
+    });
+
+
+    daysVsAmount.push(
+      {x: "20", y: amountUnder20Counter},
+      {x: "40", y: amountUnder40Counter},
+      {x: "60", y: amountUnder60Counter},
+      {x: "80", y: amountUnder80Counter},
+      {x: "100", y: amountUnder100Counter}
+    );
+
+    daysVsImpact.push(
+      {x: "20", y: impactUnder20Counter},
+      {x: "40", y: impactUnder40Counter},
+      {x: "60", y: impactUnder60Counter},
+      {x: "80", y: impactUnder80Counter},
+      {x: "100", y: impactUnder100Counter}
+    );
+
+    daysSorted.forEach((element, index) => {
+      daysVsAmountLine.push({x: index, y: element.INV_PAYMENT_AMT});
+      daysVsImpactLine.push({x: index, y: element.NEGATIVE_CASH_IMPACT});
+    });
+
+    console.log("DaysAmount");
+
+    this.setState({
+      sortedArr: daysSorted,
+      rawData: response.data[0],
+      amountArr: daysVsAmount,
+      impactArr: daysVsImpact,
+      amountArrLine: daysVsAmountLine,
+      impactArrLine: daysVsImpactLine,
+
+
+
+    }, ()=>{
+      
+    });
 
   }
 
@@ -125,11 +238,97 @@ class EarlyPayments extends Component {
   render() {
     return (
       <div>
-        <div className="GridContainer">
+        <div style={{position: "relative", width: "100%", height: "100%"}}>
           <h1>
             hello there earlyPayments
           </h1>
-          <div className="graphbox">
+          <div style={{position: "absolute", top: "5vh", right: "5vw", left: "70vw", backgroundColor: "grey"}}>
+            <svg viewBox="0 0 450 350">
+              <VictorySharedEvents
+                events={[{
+                  childName: ["pie", "bar"],
+                  target: "data",
+                  eventHandlers: {
+                    onMouseOver: () => {
+                      return [{
+                        childName: ["pie", "bar"],
+                        mutation: (props) => {
+                          return {
+                            style: Object.assign({}, props.style, {fill: "tomato"})
+                          };
+                        }
+                      }];
+                    },
+                    onMouseOut: () => {
+                      return [{
+                        childName: ["pie", "bar"],
+                        mutation: () => {
+                          return null;
+                        }
+                      }];
+                    }
+                  }
+                }]}
+              >
+                <g transform={"translate(150, 50)"}>
+                  <VictoryAxis 
+                    width={300}
+                    height={300}
+                    domain={[0, 100]}
+                    theme={VictoryTheme.material}
+                    standalone={false}
+                  />
+                  <VictoryAxis dependentAxis 
+                    width={300}
+                    height={300}
+                    domain={[0, 10]}
+                    theme={VictoryTheme.material}
+                    standalone={false}
+                  />
+                  <VictoryBar name="bar"
+                    width={300}
+                    standalone={false}
+                    style={{
+                      data: { width: 20 },
+                      labels: {fontSize: 25}
+                    }}
+                    data={this.state.amountArr}
+                    labels={["20", "40", "60", "80", "100"]}
+                    labelComponent={<VictoryLabel y={280}/>}
+                  />
+                </g>
+                <g transform={"translate(0, -75)"}>
+                  <VictoryPie name="pie"
+                    width={250}
+                    standalone={false}
+                    style={{ labels: {fontSize: 25, padding: 10}}}
+                    data={this.state.impactArr}
+                  />
+                </g>
+              </VictorySharedEvents>
+            </svg>
+          </div>
+          <div style={{position: "absolute", top: "5vh", right: "30vw", left: "45vw", backgroundColor: "grey"}}>
+            <VictoryChart width={400} height={400}>
+              <VictoryGroup
+                style={{
+                  data: { strokeWidth: 3, fillOpacity: 0.4 }
+                }}
+              >
+                <VictoryArea
+                  style={{
+                    data: { fill: "cyan", stroke: "cyan" }
+                  }}
+                  data={this.state.amountArrLine}
+                />
+                <VictoryArea
+                  style={{
+                    data: { fill: "magenta", stroke: "magenta" }
+                  }}
+                  data={this.state.impactArrLine}
+                />
+              </VictoryGroup>
+            </VictoryChart>
           </div>
         </div>
       </div>
