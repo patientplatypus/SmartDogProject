9c034b43bdd9de8d87a7e00c0950edbd51479028
Peter Weyand <pweyand@gmail.com>
2018-03-02 12:52:28 -0600

    front page done

--- src/app/components/screens/EntryPage/index.js
+++ src/app/components/screens/EntryPage/index.js
@@ -18,7 +18,11 @@ import "../../../style/fonts/fontface.css";
 import './local.css';
 import GreenCloud from '../../../style/images/GreenCloud.png';
 import DoctorSplash from '../../../style/images/doctorsplashkeyboard.jpg';
+import SmartDogLogo from '../../../style/images/smartdoglogo.png';
+import OracleLogo from '../../../style/images/ologo.png'
+import Hexagon from '../../../style/images/react-hexagon.png'
 import styled from 'styled-components';
+
 import "./grid.css"
 //
 // import XLSX from 'xlsx';
@@ -112,9 +116,46 @@ class EntryPage extends Component {
     return (
       <div>
         <div className="gridcontainer">
+          <div className="explanationcontainer2">
+            <Card title="Technical Motivation"  style={{fontSize: "2vh", height: "100%", width: "100%", overflow: "hidden", overflowY: "scroll"}}>
+              <div>
+                <p>
+                  This project currently uses React, a javascript framework, with a UI library using Ant Design and a charting library from Formidable Labs called Victory Charts. All of these tools are open source and freely available. The primary driver is React with the other tools as more of optional ways to make better styling and create the charts. For front end web design this is currently the industry standard. On the other hand, APEX has a smaller learning curve and Smart Dog has APEX trained employees. When considering going with a React site vs APEX these tradeoffs should be kept in mind.
+                </p>
+              </div>
+            </Card>
+          </div>
+          <div className="explanationcontainer1">
+            <Card title="Business Motivation Overview" style={{fontSize: "2vh", height: "100%", width: "100%", overflow: "hidden", overflowY: "scroll"}}>
+              <div>
+                <p>
+                  We have made every contained business logic need exist on a separate tab that you can navigate to at the top of the page. Inside of each tab are differing ways of pulling and viewing data from both the initially provided excel data and later of data provided from an Oracle 12g database. In a production website probably only one view will be used, however we provided variety to show some different display options.
+                </p>
+              </div>
+            </Card>
+          </div>
+          <div className="explanationcontainer3">
+            <Card title="Alert Box Explanation" style={{marginRight: "1%", fontSize: "2vh", height: "100%", overflow: "hidden", overflowY: "scroll"}}>
+              <div>
+                <p>
+                  A few of the pages have ways of sending an alert to the front page on items that need to be flagged by the user of the application - for example, because a particular vendor is overcharging. Going forward there are two principle ways of flagging data. First, a client can flag individual items and send create a report from all the tabs after the data has been preloaded. This is the route that we took. Alternatively, a flag can be set by the user to create an alert that will be pushed to the database, which will flag all new incoming data.
+                </p>
+              </div>
+            </Card>
+          </div>
+          <div className="explanationcontainer4" style={{backgroundImage: `url(${SmartDogLogo})`, backgroundRepeat: "no-repeat", backgroundSize: "50% 25%", backgroundPosition: "center"}}/>
+          <div className="explanationcontainer5" style={{backgroundImage: `url(${OracleLogo})`, backgroundRepeat: "no-repeat", backgroundSize: "47.5% 30%", backgroundPosition: "left"}}/>
+          <div className="explanationcontainer6" style={{backgroundImage: `url(${Hexagon})`, backgroundRepeat: "no-repeat", backgroundSize: "47.5% 50%", backgroundPosition: "center"}}/>
           <div className="alertboxholder">
             <Card title="Alerts!" style={{color: `#2b8ca3`, height: "100%", width: "100%", lineHeight:"2vh", marginRight: "1%", marginTop: "1%", fontSize:"10pt"}}>
               <Card title="Active Users" style={{color: `#2b8ca3`, height: "100%", width: "100%", lineHeight:"2vh", marginLeft: "1%", marginRight: "1%", fontSize:"10pt"}}>
+                {renderIf(this.state.activeUser1Name===null)(
+                  <div>
+                    <p>
+                      Currently there are no alerts for active users.
+                    </p>
+                  </div>
+                )}
                 {renderIf(this.state.activeUser1Name!=null)(
                   <div>
                     <p>
